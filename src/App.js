74a7efceff2ee1455b5ebd393abc2c6489da7624
mohsin15 <mohsin@bigappcompany.in>
2019-01-16 17:17:34 +0530

    add formikform

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {withFormik,Form} from 'formik';
+import Yup from 'yup';
 
 class App extends Component {
+
   render() {
+    const {values, handleChange, handleSubmit} = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Form>
+        <input type="email" name="email" placeholder="Email" value={values.email} onChange={handleChange}  />
+        <input type="password" name="password" placeholder="password" value={values.password} onChange={handleChange}  />
+        <button>Submit</button>
+        
+      </Form>
     );
   }
 }
 
-export default App;
+const FormikApp = withFormik({
+  mapPropsToValues({email, password}){
+    return {
+      email: email || '',
+      password: password || ''
+    }
+  },
+  handleSubmit(values){
+    console.log(values);
+  }
+})(App)
+
+export default FormikApp;
